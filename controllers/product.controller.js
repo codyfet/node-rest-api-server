2f125d128469ca1ccd38b177ca97d81f8d082a1f
codyfet <codyfet@mail.ru>
2018-12-14 02:31:34 +0300

    Реализовал сервис все продукты

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,28 +1,54 @@
 const Product = require('../models/product.model');
 
+/**
+ * Создать запись.
+ */
 module.exports.product_create = function (req, res, next) {
-    let product = new Product(
-        {
+    Product.create({
+        name: req.body.name,
+        price: req.body.price
+    }, function (err) {
+        if (err) return next(err);
+        res.send('Product created successfully!');
+    });
+    
+    /*
+        Можно создать запись и так:
+
+        let product = new Product({
             name: req.body.name,
             price: req.body.price
-        }
-    );
+        });
 
-    product.save(function (err) {
-        if (err) {
-            return next(err);
-        }
-        res.send('Product Created successfully')
-    })
+        product.save(function (err) {
+            if (err) return next(err);
+            res.send('Product Created successfully')
+        });
+    */
 };
 
+/**
+ * Получить список всех записей.
+ */
+module.exports.product_all_details = function (req, res) {
+    Product.find({}, (err, items) => {
+        res.send(items);
+    });
+}
+
+/**
+ * Получить запись по id.
+ */
 module.exports.product_details = function (req, res, next) {
     Product.findById(req.params.id, function (err, product) {
         if (err) return next(err);
         res.send(product);
-    })
+    });
 };
 
+/**
+ * Изменить запись по id.
+ */
 module.exports.product_update = function (req, res, next) {
     Product.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err, product) {
         if (err) return next(err);
@@ -30,19 +56,12 @@ module.exports.product_update = function (req, res, next) {
     });
 };
 
+/**
+ * Удалить запись по id.
+ */
 module.exports.product_delete = function (req, res, next) {
     Product.findByIdAndRemove(req.params.id, function (err) {
         if (err) return next(err);
         res.send('Deleted successfully!');
-    })
+    });
 };
-
-// module.exports.product_all_details = function (req, res, next) {
-//     Product.find({}, function (err, items) {
-//         var itemsMap = {};
-//         items.forEach(function (item) {
-//             itemsMap[item._id] = item;
-//         });
-//         res.send(itemsMap);
-//     });
-// }
--- routes/product.route.js
+++ routes/product.route.js
@@ -3,15 +3,29 @@ const router = express.Router();
 
 const product_controller = require('../controllers/product.controller');
 
+/**
+ * Создать запись.
+ */
 router.post('/create', product_controller.product_create);
 
+/**
+ * Получить список всех записей.
+ */
+router.get('/', product_controller.product_all_details);
+
+/**
+ * Получить запись по id.
+ */
 router.get('/:id', product_controller.product_details);
 
-// TODO Хорошо бы добавить такой сервис: получить все.
-// router.get('/list', product_controller.product_all_details);
-
+/**
+ * Изменить апись по id.
+ */
 router.put('/:id/update', product_controller.product_update);
 
+/**
+ * Удалить запись по id.
+ */
 router.delete('/:id/delete', product_controller.product_delete);
 
 module.exports = router;
